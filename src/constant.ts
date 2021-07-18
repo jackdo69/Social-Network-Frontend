b4671ef936494ec001caabb1df85b220683fbd3a
Jack Do <jackie.duc.do@gmail.com>
2021-07-18 23:39:06 +0700

    using the material ui admin theme

--- src/constant.ts
+++
@@ -1,5 +0,0 @@
-const ACCESS_TOKEN = 'access_token';
-const REFRESH_TOKEN = 'refresh_token';
-const BASE_URL = 'http://localhost:4000';
-
-export { ACCESS_TOKEN, REFRESH_TOKEN, BASE_URL };
--- src/pages/User.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from '../hoc/Layout';
-import Profile from '../components/Profile';
-
-export default function User() {
-  const profile = <Profile />;
-  return <Layout leftSideBar={profile} main={'Personal Post'} rightSideBar={'Chatbox'} />;
-}
